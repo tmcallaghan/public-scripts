3866f5a491a61c428579d76596fd64fa6ab99cc7
Tim Callaghan <tmcallaghan@gmail.com>
2021-04-07 16:15:28 -0400

    adding amazon linux 2 download

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -4,3 +4,4 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.4.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.13.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon2-4.4.4.tgz
