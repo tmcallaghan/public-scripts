754d6e45c97cfce188982c61d3f78d38b16e44be
Tim Callaghan <tmcallaghan@gmail.com>
2021-08-05 09:43:56 -0400

    Adding mongodb shell

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -5,3 +5,4 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.13.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon2-4.4.4.tgz
+wget https://downloads.mongodb.com/compass/mongosh-1.0.4-linux-x64.tgz
