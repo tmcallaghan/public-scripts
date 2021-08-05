2ed5291515eb554fd4cf818b5641c494758dde62
Tim Callaghan <tmcallaghan@gmail.com>
2021-08-05 09:44:51 -0400

    Adding MongoDB 5.0

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.2.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.4.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.13.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
