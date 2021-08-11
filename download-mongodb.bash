1aae7981965022c5386e83c36bcd47520424b100
Tim Callaghan <tmcallaghan@gmail.com>
2021-08-11 08:48:31 -0400

    Adding tar extraction

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -7,3 +7,11 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon2-4.4.4.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.0.4-linux-x64.tgz
+
+tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.2.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu2004-4.4.4.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu1804-4.2.13.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
+tar xzvf mongodb-linux-x86_64-amazon2-4.4.4.tgz
+tar xzvf mongosh-1.0.4-linux-x64.tgz
