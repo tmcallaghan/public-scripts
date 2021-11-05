4a07c40faa5341defe31d214e518392c641a77bc
Tim Callaghan <tmcallaghan@gmail.com>
2021-11-05 09:43:45 -0400

    Updating version for MongoDB 5.0

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.2.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.3.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.4.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.13.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
@@ -9,7 +9,7 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon2-4.4.4.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.12.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.0.4-linux-x64.tgz
 
-tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.2.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.3.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-4.4.4.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.2.13.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
