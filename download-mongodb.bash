c2596f0add8687a0ef5f6e049a4b9c43cde7d8ec
Tim Callaghan <tmcallaghan@gmail.com>
2022-03-25 15:03:29 -0400

    Adding MongoDB 3.4.24

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -5,6 +5,7 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.12.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.18.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.24.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.12.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.0.4-linux-x64.tgz
 
@@ -13,5 +14,6 @@ tar xzvf mongodb-linux-x86_64-ubuntu2004-4.4.12.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.2.18.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
+tar xzvf mongodb-linux-x86_64-3.4.24.tgz
 tar xzvf mongodb-linux-x86_64-2.6.12.tgz
 tar xzvf mongosh-1.0.4-linux-x64.tgz
