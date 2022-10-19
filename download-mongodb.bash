4c5a504a207de56820187a02b259fd44129bdd92
Tim Callaghan <tmcallaghan@gmail.com>
2022-10-19 09:04:58 -0400

    Updating for current versions

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-6.0.0-rc10.tgz
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.9.tgz
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.15.tgz
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.21.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-6.0.2.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.13.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.17.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.24.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.12.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.5.0-linux-x64.tgz
 
-tar xzvf mongodb-linux-x86_64-ubuntu2004-6.0.0-rc10.tgz
-tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.9.tgz
-tar xzvf mongodb-linux-x86_64-ubuntu2004-4.4.15.tgz
-tar xzvf mongodb-linux-x86_64-ubuntu1804-4.2.21.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu2004-6.0.2.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.13.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu2004-4.4.17.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu1804-4.2.23.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 tar xzvf mongodb-linux-x86_64-3.4.24.tgz
