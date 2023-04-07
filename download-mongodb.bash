2a8f6cbc62ae395ee0f3ac7357db4709af5f3641
Tim Callaghan <tmcallaghan@gmail.com>
2023-04-07 11:45:15 -0400

    adding database tools

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -9,6 +9,7 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.24.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.12.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.5.0-linux-x64.tgz
+wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.7.0.tgz
 
 tar xzvf mongodb-linux-x86_64-ubuntu2004-6.0.3.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.13.tgz
@@ -19,3 +20,4 @@ tar xzvf mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 tar xzvf mongodb-linux-x86_64-3.4.24.tgz
 tar xzvf mongodb-linux-x86_64-2.6.12.tgz
 tar xzvf mongosh-1.5.0-linux-x64.tgz
+tar xzvf tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.7.0.tgz
