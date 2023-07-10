1ec1e22795252522fb2be719e6542118f96da2a8
Tim Callaghan <tmcallaghan@gmail.com>
2023-07-10 10:15:56 -0400

    adding MongoDB 3.0

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -7,6 +7,7 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.24.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.15.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.12.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.5.0-linux-x64.tgz
 wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.7.0.tgz
@@ -18,6 +19,7 @@ tar xzvf mongodb-linux-x86_64-ubuntu1804-4.2.23.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-3.6.23.tgz
 tar xzvf mongodb-linux-x86_64-3.4.24.tgz
+tar xzvf mongodb-linux-x86_64-3.0.15.tgz
 tar xzvf mongodb-linux-x86_64-2.6.12.tgz
 tar xzvf mongosh-1.5.0-linux-x64.tgz
 tar xzvf mongodb-database-tools-ubuntu2004-x86_64-100.7.0.tgz
