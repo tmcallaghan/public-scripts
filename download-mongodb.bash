e81c4ad81474e628f81f2ef5cbce67d07c4fbd13
Tim Callaghan <tmcallaghan@gmail.com>
2023-08-03 13:15:48 -0400

    Updating tools to 100.7.4

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -10,7 +10,7 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.24.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.15.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.12.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.5.0-linux-x64.tgz
-wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.7.0.tgz
+wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.7.4.tgz
 
 tar xzvf mongodb-linux-x86_64-ubuntu2004-6.0.3.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.13.tgz
@@ -22,4 +22,4 @@ tar xzvf mongodb-linux-x86_64-3.4.24.tgz
 tar xzvf mongodb-linux-x86_64-3.0.15.tgz
 tar xzvf mongodb-linux-x86_64-2.6.12.tgz
 tar xzvf mongosh-1.5.0-linux-x64.tgz
-tar xzvf mongodb-database-tools-ubuntu2004-x86_64-100.7.0.tgz
+tar xzvf mongodb-database-tools-ubuntu2004-x86_64-100.7.4.tgz
