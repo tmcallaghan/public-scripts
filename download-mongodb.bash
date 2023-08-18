baadf6998b7f21894d59e9a8ad3aaed75f6c4b91
Tim Callaghan <tmcallaghan@gmail.com>
2023-08-18 13:27:58 -0400

    Adding MongoDB 7.0

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-7.0.0.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-6.0.3.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.13.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.17.tgz
@@ -12,6 +13,7 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.12.tgz
 wget https://downloads.mongodb.com/compass/mongosh-1.5.0-linux-x64.tgz
 wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.7.4.tgz
 
+tar xzvf mongodb-linux-x86_64-ubuntu2004-7.0.0.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-6.0.3.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.13.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-4.4.17.tgz
