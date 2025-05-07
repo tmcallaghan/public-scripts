49ec3f306142b9b4fb28ff25ff2e051283947331
Tim Callaghan <tmcallaghan@gmail.com>
2025-05-07 15:36:01 +0000

    updating mongodb v5 version

--- download-mongodb.bash
+++ download-mongodb.bash
@@ -3,7 +3,7 @@
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2404-8.0.9.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-7.0.20.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-6.0.23.tgz
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.28.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.31.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.17.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.23.tgz
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
@@ -22,7 +22,7 @@ wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_6
 tar xzvf mongodb-linux-x86_64-ubuntu2404-8.0.9.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-7.0.20.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-6.0.23.tgz
-tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.28.tgz
+tar xzvf mongodb-linux-x86_64-ubuntu2004-5.0.31.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu2004-4.4.17.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.2.23.tgz
 tar xzvf mongodb-linux-x86_64-ubuntu1804-4.0.28.tgz
